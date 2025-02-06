4cfa83eb63a32cc923735f6853d6cd99e977e5fa
Lind <[lind_zariqi@hotmail.com]>
2025-02-06 04:18:39 +0000

    Fixed mixed-content error

--- scripts/autoRefresh.js
+++ scripts/autoRefresh.js
@@ -16,7 +16,7 @@ function startCountdown() {
     countdownInterval = setInterval(() => {
         timeLeft--;
         autoRefreshButton.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                 <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 11A8.1 8.1 0 0 0 4.5 9M4 5v4h4m-4 4a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"/>
             </svg>
             Disable Auto Refresh (${timeLeft}s)
@@ -25,7 +25,7 @@ function startCountdown() {
         if (timeLeft <= 0) {
             clearInterval(countdownInterval); // Stop the countdown when it reaches 0
             autoRefreshButton.innerHTML = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                     <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 11A8.1 8.1 0 0 0 4.5 9M4 5v4h4m-4 4a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"/>
                 </svg>
                 Disable Auto Refresh
@@ -39,7 +39,7 @@ function stopCountdown() {
     clearInterval(countdownInterval); // Stop the countdown
     const autoRefreshButton = document.getElementById("auto-refresh-toggle");
     autoRefreshButton.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <svg xmlns="https://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 11A8.1 8.1 0 0 0 4.5 9M4 5v4h4m-4 4a8.1 8.1 0 0 0 15.5 2m.5 4v-4h-4"/>
         </svg>
         Enable Auto Refresh (30s)
